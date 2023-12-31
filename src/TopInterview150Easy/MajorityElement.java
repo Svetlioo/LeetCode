46bd21378d25fc8ebac2090f36afebaa7731b0e0
Svetoslav Iliev <svetlio.iliev17@gmail.com>
2023-12-31 19:43:02 +0200

    Solved MajorityElement Problem

---
+++ src/TopInterview150Easy/MajorityElement.java
@@ -0,0 +1,26 @@
+package TopInterview150Easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MajorityElement {
+    public static void main(String[] args) {
+        int[] nums = new int[]{2, 2, 1, 1, 1, 2, 2};
+        System.out.println(majorityElement(nums));
+    }
+
+    public static int majorityElement(int[] nums) {
+        int n = nums.length;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        n /= 2;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > n) {
+                return entry.getKey();
+            }
+        }
+        return 0;
+    }
+}
